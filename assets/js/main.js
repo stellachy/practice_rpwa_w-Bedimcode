81c755d05e5f91071c32655478189b840aacb68c
stellachy <124666701+stellachy@users.noreply.github.com>
2023-02-19 16:10:51 +0800

    Add files via upload
    finished ##skills and added ##qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,52 @@ for(n=0; n<navLink.length; n++){
 */
 
 /*accordion skills*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+      skillsHeader = document.querySelectorAll(".skills__header")
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i< skillsContent.length; i++){
+        skillsContent[i].className = "skills__content skills__close"
+    }
+
+    if(itemClass == "skills__content skills__close"){
+        this.parentNode.className = "skills__content skills__open"
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener("click", toggleSkills)
+})
 
 /*qualification tabs*/
+const tabs = document.querySelectorAll("[data-target]"),
+      tabContents = document.querySelectorAll("[data-content]")
 
+tabs.forEach(tab =>{
+    tab.addEventListener("click", () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContents =>{
+            tabContents.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+
+        tabs.forEach(tab =>{
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+        
+    })
+})
 /*service modal*/  
+
+/*portfolio swiper*/
+
+/*testimonial*/
+
+/*scroll sections active link*/
+
+/*change background header*/
+
